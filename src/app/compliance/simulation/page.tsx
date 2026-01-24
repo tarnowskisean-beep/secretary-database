8cbbf3de978969a80a78945704b24dcdd780aeb2
Sean Tarnowski <seantarnowski@Mac.lan>
2026-01-23 22:08:53 -0800

    Force dynamic rendering for simulation page to bypass build-time DB check

--- src/app/compliance/simulation/page.tsx
+++ src/app/compliance/simulation/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/db'
 import SimulationClient from './SimulationClient'
 import Link from 'next/link'
+export const dynamic = 'force-dynamic'
 
 export default async function SimulationPage() {
     const people = await prisma.person.findMany({
